257d002f0e5813708077d9d7af779c35b0a99b60
larugíta <nassimwarner07@gmail.com>
2026-07-18 08:54:49 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            while(!Character.isLetterOrDigit(s.charAt(i)) && i < j) i++;
+            while(!Character.isLetterOrDigit(s.charAt(j)) && i < j) j--;
+
+            if(s.charAt(i) != s.charAt(j)) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+        
+    }
+}
