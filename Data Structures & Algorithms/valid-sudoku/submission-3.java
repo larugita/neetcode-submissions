bdd3235c9230d87dadeef27262c5558cff8791be
larugíta <nassimwarnerr@gmail.com>
2026-07-29 10:34:48 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        for(int i = 0; i < 9; i++){
+            HashSet<Character> set = new HashSet<>();
+            for(int j = 0; j < 9; j++){
+                char c = board[i][j];
+
+                if(c == '.') continue;
+
+                if(set.contains(c)){
+                    return false;
+                }
+                set.add(c);
+            }
+        }
+
+        for(int i = 0; i < 9; i++){
+            HashSet<Character> set = new HashSet<>();
+            for(int j = 0; j < 9; j++){
+                char c = board[j][i];
+
+                if(c == '.') continue;
+
+                if(set.contains(c)){
+                    return false;
+                }
+                set.add(c);
+            }
+        }
+
+        for(int boxrow = 0; boxrow < 9; boxrow+=3){
+            for(int boxcol = 0; boxcol < 9; boxcol += 3){
+                HashSet <Character> box = new HashSet<>();
+                for(int i = 0; i < 3; i++){
+                    for(int j = 0; j < 3; j++){
+                        char c = board[boxrow + i][boxcol + j];
+                        if(c == '.') continue;
+                        if(box.contains(c)) return false;
+                        box.add(c);
+                    }
+                }
+            }
+        }
+
+        return true;
+        
+    }
+}
