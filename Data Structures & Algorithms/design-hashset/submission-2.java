be3fc4df5c5684441e6786672e774a7347fce31a
larugíta <nassimwarnerr@gmail.com>
2026-08-09 18:29:38 -0400

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.java
@@ -0,0 +1,20 @@
+class MyHashSet {
+
+    boolean[] data;
+
+    public MyHashSet() {
+        data = new boolean[10000000];
+    }
+
+    public void add(int key) {
+        data[key] = true;
+    }
+
+    public void remove(int key) {
+        data[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return data[key];
+    }
+}
